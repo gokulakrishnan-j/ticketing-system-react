34ab9397cc895c66db1f3997052d82205ca82241
GOKULAKRISHNAN . J <110898672+gokulakrishnan-j@users.noreply.github.com>
2023-01-02 13:16:22 +0530

    Update UserSignup.js

--- src/components/UserSignup.js
+++ src/components/UserSignup.js
@@ -64,8 +64,10 @@ var  time = ''
 if(h === 0){
  time =  12 + ':' + m  + ':'+s +' ' + 'AM'
 }
-else if(h < 13){
+else if(h < 12){
   time =  h  + ':' + m  + ':'+s +' ' + 'AM'
+}else if(h === 12){
+  time =  h  + ':' + m  + ':'+s +' ' + 'PM'
 }
 else{
    time =  h -12 + ':' + m  + ':'+s +' '+ 'PM'
